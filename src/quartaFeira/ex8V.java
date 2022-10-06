e1d18cd69b4e2f88b4b8bf5da229766a8e6a58e1
daviravezeiro <daviboff783@gmail.com>
2022-10-05 21:11:51 -0300

    update

---
+++ src/quartaFeira/ex8V.java
@@ -0,0 +1,21 @@
+package quartaFeira;
+
+import javax.swing.JOptionPane;
+
+public class ex8V {
+
+	public static void main(String[] args) {
+		int vet[] = new int[3];
+		Integer soma = 0;
+		for (int i = 0; i < vet.length; i++) {
+			String num = JOptionPane.showInputDialog("Insira um numero: ");
+			Integer num1 = Integer.valueOf(num);
+			vet[i] = num1;
+			soma += vet[i];
+
+		}
+		JOptionPane.showMessageDialog(null, "Resultado da soma = "+soma);
+
+	}
+
+}
